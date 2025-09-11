6b38dc5e98ab13064391197e9e2645d08b118e0f
Tiago Ramos <tiraramos@hotmail.com>
2025-09-11 15:29:52 -0300

    Fixes Docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000',
   },
+  // Permitir cross-origin para VPN
+  allowedDevOrigins: ['192.168.10.35:3000'],
   async rewrites() {
     return [
       {
